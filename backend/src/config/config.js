173047a89afa88366799f686b0b6b033b4ee55f7
shahayush457 <shahayush457@gmail.com>
2021-03-02 13:02:11 +0530

    OAuth configs added

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -5,5 +5,16 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 module.exports = {
   app: process.env.APP_ID,
   port: process.env.PORT,
-  db_url: process.env.DB_URL
+  db_url: process.env.DB_URL,
+  jwt_secret: process.env.jwt,
+  oauth: {
+    google: {
+      clientId: process.env.google_clientId,
+      clientSecret: process.env.google_clientSecret
+    },
+    facebook: {
+      clientId: process.env.facebook_clientId,
+      clientSecret: process.env.facebook_clientSecret
+    }
+  }
 };
