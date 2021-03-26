ae46dcfcd27be04489b37cb92d69b741a0c75f81
shahayush457 <shahayush457@gmail.com>
2021-03-26 22:47:05 +0530

    Added more details in email reminder

--- backend/src/jobScheduler/sendReminder.js
+++ backend/src/jobScheduler/sendReminder.js
@@ -1,7 +1,7 @@
 let nodemailer = require("nodemailer");
 const { email, password } = require("../config/config");
 
-function createText(transactionType, amount, entityName) {
+function createPaymentText(transactionType, amount, entityName) {
   return transactionType === "debit"
     ? `You have a pending payment of Rs ${amount} to ${entityName}`
     : `You have a pending payment of Rs ${amount} from ${entityName}`;
@@ -17,13 +17,38 @@ module.exports = async agenda => {
   });
 
   agenda.define("send email reminder", async job => {
-    const { to, transactionType, amount, entityName } = job.attrs.data;
+    const {
+      to,
+      transactionType,
+      transactionMode,
+      amount,
+      remarks,
+      entityName,
+      entityType,
+      entityAddress,
+      entityContactNo
+    } = job.attrs.data;
 
     const mailOptions = {
       from: email,
       to: to,
       subject: "Reminder, pending transaction",
-      text: createText(transactionType, amount, entityName)
+      text: `<h1>You have a pending transaction.</h1>
+             <h2>Transaction details</h2>
+             <ol type="1">
+             <li>Amount - ${amount}</li>
+             <li>Transaction type - ${transactionType}</li>
+             <li>Transaction mode - ${transactionMode}</li>
+             <li>Remarks - ${remarks}</li>
+             </ol>
+             <h2>Entity details</h2>
+             <ol type="1">
+             <li>Name - ${entityName}</li>
+             <li>Entity Type - ${entityType}</li>
+             <li>Address - ${entityAddress}</li>
+             <li>Contact No - ${entityContactNo}</li>
+             </ol>
+             `
     };
 
     transporter.sendMail(mailOptions, function(error, info) {
