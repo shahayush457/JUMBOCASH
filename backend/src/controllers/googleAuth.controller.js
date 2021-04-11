9dc21da14ed7b4420bc67a409d564732f93fecb1
shahayush457 <shahayush457@gmail.com>
2021-04-11 17:49:40 +0530

    Deleted unused files

--- backend/src/controllers/googleAuth.controller.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-const { jwt_secret } = require("../config/config");
-
-exports.googleOAuth = async (req, res, next) => {
-  const { id, name } = req.user;
-  // Generate token
-  const token = jwt.sign({ id, name }, jwt_secret);
-  res.status(200).json({ id, name, token });
-};
